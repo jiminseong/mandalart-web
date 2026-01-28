3b0bfbccddac9f7e19bc44a982059f8edc3a74c6
jiminseong <iamjms4237@gachon.ac.kr>
2026-01-28 22:45:25 +0900

    chore: robots.ts 및 sitemap.ts 파일에서 도메인 URL에 www를 추가했습니다.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/api/",
     },
-    sitemap: "https://mandalart.life/sitemap.xml",
+    sitemap: "https://www.mandalart.life/sitemap.xml",
   };
 }
